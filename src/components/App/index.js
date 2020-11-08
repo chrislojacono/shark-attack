ee05da0fbf1f8a57b81a32a10705efadb626d49c
Chris LoJacono <clojacono92@gmail.com>
2020-11-08 14:45:26 -0600

    file structure setup

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.scss';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
+        <h2>Shark Tank</h2>
         <button className="btn btn-info">I am a button</button>
       </div>
     );
